4bc788d680d7cca63ae888586e9edac1a6aa55e7
DennisCcs <dennisccamasaico@gmail.com>
2026-08-14 10:04:47 -0500

    ejercisio 5

---
+++ RepaGrupo1_Array/src/main/java/pe/edu/upeu/repagrupo1_array/ArraysG_1.java
@@ -0,0 +1,42 @@
+package pe.edu.upeu.repagrupo1_array;
+public class ArraysG_1 {
+    public static int[] ejemploVector(){
+        int[] v = {15,20,16,12,8};
+        System.out.println(v.length);
+        v[2] = 10;
+        return  v;
+    }
+    public static void imprimeVector(int[] v){
+        for (int i = 0; i < v.length;i ++){
+            System.out.printf(v[i]+"\t");
+        }
+    }
+
+    public static void imprimeMatriz(int[][] m){
+        for (int f=0;f<m.length;f++){
+            for (int c=0;c<m[0].length;c++){
+                System.out.print(m[f][c]+"\t");
+            }
+            System.out.println("");
+        }
+    }
+
+    public static int[][] matrizF5(int tam , int numI){
+        int[][] m = new int[tam][tam];
+        for (int f = 0; f < m.length; f++) {
+            for (int c = m[0].length-1; c >= m[0].length-1-f; c--) {
+                m[f][c]=numI;
+                numI++;
+            }
+        }
+        return m;
+    }
+
+    public static void main(String[] args) {
+        //imprimeVector(ejemploVector());
+        System.out.println("");
+        int[][] m = {{2,3},{4,6},{9,10}};
+        //imprimeMatriz(m);
+        imprimeMatriz(matrizF5(5,0));
+    }
+}
