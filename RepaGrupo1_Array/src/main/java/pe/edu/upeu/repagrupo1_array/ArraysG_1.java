5856430b8eb99c29b3eb80b8a2ed5f8bc8fc0929
DennisCcs <dennisccamasaico@gmail.com>
2026-08-21 08:19:45 -0500

    Matriz en GUI forma5

--- RepaGrupo1_Array/src/main/java/pe/edu/upeu/repagrupo1_array/ArraysG_1.java
+++ RepaGrupo1_Array/src/main/java/pe/edu/upeu/repagrupo1_array/ArraysG_1.java
@@ -32,11 +32,24 @@ public class ArraysG_1 {
         return m;
     }
 
+    public static int[][] matrizF6(int tam, int num1){
+        int[][] m = new int[tam][tam];
+        for (int f = 0; f < m.length; f++) {
+            for (int c = m[0].length-1; c >= m[0].length-1-f; c++) {
+            m[f][c]=num1;
+            num1++;
+            }
+
+        }
+        return m;
+    }
+
     public static void main(String[] args) {
         //imprimeVector(ejemploVector());
         System.out.println("");
         int[][] m = {{2,3},{4,6},{9,10}};
         //imprimeMatriz(m);
+        imprimeMatriz(matrizF5(5,0));
         imprimeMatriz(matrizF5(5,0));
     }
 }
